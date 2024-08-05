7e6b7bc447b8d74dd18b4369f56bcb803a76d486
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-08-05 17:49:56 +0300

    add files

--- src/main/java/org/petproject/App.java
+++ src/main/java/org/petproject/App.java
@@ -1,5 +1,7 @@
 package org.petproject;
 
+import java.sql.*;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try
+                (
+                        // create a database connection
+                        Connection connection = DriverManager.getConnection("jdbc:sqlite:currency.db");
+                        Statement statement = connection.createStatement();
+                )
+        {
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+            ResultSet rs = statement.executeQuery("select * from currencies");
+            while(rs.next())
+            {
+                // read the result set
+                System.out.println("Code = " + rs.getString("Code"));
+                System.out.println("FullName = " + rs.getString("FullName"));
+            }
+        }
+        catch(SQLException e)
+        {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            e.printStackTrace(System.err);
+        }
     }
 }
