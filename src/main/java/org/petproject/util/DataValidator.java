2fded198aee23dae67eb48a7810034f046e50384
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-09-25 17:58:37 +0300

    add validate rate

--- src/main/java/org/petproject/util/DataValidator.java
+++ src/main/java/org/petproject/util/DataValidator.java
@@ -5,4 +5,14 @@ public class DataValidator {
     public static boolean isCurrencyCodeNotValid(String code) {
         return code == null || code.length() != 3 || !code.matches("^[A-Z]+$");
     }
+
+
+    public static boolean isCurrenciesCodesNotValid(String code) {
+        return code == null || code.length() != 6 || !code.matches("^[A-Z]+$");
+    }
+
+
+    public static boolean isExchangeRateNotValid(String rate) {
+        return !rate.matches("^[0-9]+\\.?[0-9]{0,6}$");
+    }
 }
