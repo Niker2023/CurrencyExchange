b176608cfbbfdf63c6da521ae6bdd73b4fae6208
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-09-27 10:08:16 +0300

    currency mappers combined into one class

--- src/main/java/org/petproject/mapper/CurrencyMapper.java
+++ src/main/java/org/petproject/mapper/CurrencyMapper.java
@@ -6,7 +6,8 @@ import org.petproject.dto.CurrencyDto;
 import org.petproject.entity.Currency;
 
 @Mapper
-public interface DtoToCurrencyMapper {
-    DtoToCurrencyMapper INSTANCE = Mappers.getMapper(DtoToCurrencyMapper.class);
-    Currency toCurrency(CurrencyDto dto);
+public interface CurrencyMapper {
+    CurrencyMapper INSTANCE = Mappers.getMapper(CurrencyMapper.class);
+    CurrencyDto toDto(Currency currency);
+    Currency fromDto(CurrencyDto dto);
 }
