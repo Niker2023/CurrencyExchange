d0b4de9f2bdcf44f2a52fed35f168dcdea72f018
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-09-27 12:41:14 +0300

    added custom mappers for mapper toExchangeRate and toExchangeRateDto

--- src/main/java/org/petproject/mapper/ExchangeRateMapper.java
+++ src/main/java/org/petproject/mapper/ExchangeRateMapper.java
@@ -2,15 +2,41 @@ package org.petproject.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 import org.petproject.dto.CurrencyDto;
 import org.petproject.dto.ExchangeRateDto;
 import org.petproject.entity.ExchangeRate;
+import org.petproject.service.CurrencyService;
+
+import java.sql.SQLException;
 
 @Mapper(uses = {CurrencyMapper.class})
 public interface ExchangeRateMapper {
     ExchangeRateMapper INSTANCE = Mappers.getMapper(ExchangeRateMapper.class);
-    ExchangeRateDto toExchangeRateDto(ExchangeRate rate);
-    @Mapping(source = "ExchangeRate.baseCurrencyId", target = "CurrencyDto")
+
+
+    @Mapping(source = "baseCurrency", target = "baseCurrencyId", qualifiedByName = "getCurrencyIdByCurrency")
+    @Mapping(source = "targetCurrency", target = "targetCurrencyId", qualifiedByName = "getCurrencyIdByCurrency")
     ExchangeRate toExchangeRate(ExchangeRateDto dto);
+
+    @Mapping(source = "baseCurrencyId", target = "baseCurrency", qualifiedByName = "getCurrencyDtoById")
+    @Mapping(source = "targetCurrencyId", target = "targetCurrency", qualifiedByName = "getCurrencyDtoById")
+    ExchangeRateDto toExchangeRateDto(ExchangeRate exchangeRate) throws SQLException;
+
+
+    @Named("getCurrencyIdByCurrency")
+    public static int getCurrencyIdByCurrency(CurrencyDto currency) {
+        return currency.getId();
+    }
+
+
+    @Named("getCurrencyDtoById")
+    public static CurrencyDto getCurrencyDtoById(int id) throws SQLException {
+        var currencyDtoById = CurrencyService.getInstance().getCurrencyDtoById(id);
+        if (currencyDtoById.isPresent()) {
+            return currencyDtoById.get();
+        }
+        throw new SQLException("Currency not found");
+    }
 }
