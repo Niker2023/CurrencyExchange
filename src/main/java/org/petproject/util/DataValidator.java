c08fa069f717f05b74890e703065905c020c64f2
Niker2023 <berserg@bk.ru>
2024-10-06 23:40:24 +0300

    added a null check

--- src/main/java/org/petproject/util/DataValidator.java
+++ src/main/java/org/petproject/util/DataValidator.java
@@ -13,6 +13,6 @@ public class DataValidator {
 
 
     public static boolean isExchangeRateNotValid(String rate) {
-        return !(rate.matches("^[0-9]+(\\.[0-9])?[0-9]{0,5}$") && Double.parseDouble(rate) != 0);
+        return rate == null || !(rate.matches("^[0-9]+(\\.[0-9])?[0-9]{0,5}$") && Double.parseDouble(rate) != 0);
     }
 }
