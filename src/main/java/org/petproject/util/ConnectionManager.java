bdad0d08c7afc4279a942602e455b4014203c67b
Niker2023 <berserg@bk.ru>
2024-10-10 12:09:40 +0300

    added pool connection HikariCP

--- src/main/java/org/petproject/util/ConnectionManager.java
+++ src/main/java/org/petproject/util/ConnectionManager.java
@@ -1,17 +1,23 @@
 package org.petproject.util;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public final class ConnectionManager {
 
     private static final String PATH_TO_DB = "/home/nikita/IdeaProjects/CurrencyExchange/currency.db";
-//    private static final String PATH_TO_DB = "/home/currency.db";
+//    private static final String PATH_TO_DB = "/home/currency.db";  // for test 500 error
+    private static final HikariConfig config = new HikariConfig();
+    private static HikariDataSource dataSource;
+
 
     static {
         loadDriver();
     }
+
 
     private static void loadDriver() {
         System.setProperty("java.io.tmpdir", "/var/tmp");
@@ -22,13 +28,19 @@ public final class ConnectionManager {
         }
     }
 
+
     private ConnectionManager() {
     }
 
+
     public static Connection get() throws SQLException {
         try {
-            return DriverManager.getConnection("jdbc:sqlite:" + PATH_TO_DB);
-        } catch (SQLException e) {
+            if (dataSource == null) {
+                config.setJdbcUrl("jdbc:sqlite:" + PATH_TO_DB);
+                dataSource = new HikariDataSource(config);
+            }
+            return dataSource.getConnection();
+        } catch (Exception e) {
             throw new SQLException(e);
         }
     }
