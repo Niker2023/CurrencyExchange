495cb88b7f64a93746c2ad6a7a0172797bb872ae
Niker2023 <berserg@bk.ru>
2024-10-14 00:16:48 +0300

    add cors support

---
+++ src/main/java/org/petproject/filter/CorsFilter.java
@@ -0,0 +1,20 @@
+package org.petproject.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class CorsFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
