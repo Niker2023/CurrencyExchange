5584456bccc030720495ca55097bbd9c561efec7
Niker2023 <berserg@bk.ru>
2024-10-15 00:20:44 +0300

    fix format

--- src/main/java/org/petproject/servlet/CurrenciesServlet.java
+++ src/main/java/org/petproject/servlet/CurrenciesServlet.java
@@ -38,9 +38,7 @@ public class CurrenciesServlet extends HttpServlet {
         var code = req.getParameter("code");
         var sign = req.getParameter("sign");
 
-        if (name == null || name.isBlank() ||
-                sign == null || sign.isBlank() ||
-                DataValidator.isCurrencyCodeNotValid(code)) {
+        if (name == null || name.isBlank() || sign == null || sign.isBlank() || DataValidator.isCurrencyCodeNotValid(code)) {
             resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             resp.getWriter().write(gson.toJson(new ErrorResponse("The required form field is missing or incorrect.")));
             return;
